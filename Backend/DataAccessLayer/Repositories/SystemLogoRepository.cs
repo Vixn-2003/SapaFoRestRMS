a6e8100a6cc63dd60ba6eb23cc7004e0eefc4322
Luongnd <luongndhe170199@fpt.edu.vn>
2025-10-05 16:10:09 +0700

    edit back end  and add fontend for manager logo

--- Backend/DataAccessLayer/Repositories/SystemLogoRepository.cs
+++ Backend/DataAccessLayer/Repositories/SystemLogoRepository.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccessLayer.Dbcontext;
 using DataAccessLayer.Repositories.Interfaces;
 using DomainAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,16 +20,16 @@ namespace DataAccessLayer.Repositories
         }
         public IEnumerable<SystemLogo> GetAll()
         {
-            return _context.SystemLogos.ToList();
+            return _context.SystemLogos.Include(l => l.CreatedByNavigation).ToList();
         }
 
         public IEnumerable<SystemLogo> GetActiveLogos()
         {
-            return _context.SystemLogos.Where(l => l.IsActive == true).ToList();
+            return _context.SystemLogos.Include(l => l.CreatedByNavigation).Where(l => l.IsActive == true).ToList();
         }
         public async Task<SystemLogo?> GetByIdAsync(int id)
         {
-            return await _context.SystemLogos.FindAsync(id);
+            return await _context.SystemLogos.Include(l => l.CreatedByNavigation).FirstOrDefaultAsync(l => l.LogoId == id);
         }
 
         public async Task AddAsync(SystemLogo logo)
