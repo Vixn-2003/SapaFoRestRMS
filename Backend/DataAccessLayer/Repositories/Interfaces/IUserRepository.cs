a9f8ac933cfcfbb11ce67e45b2ba121c8acfd710
Vi Nguyen <xuanvidz03102003@gmail.com>
2025-10-05 16:39:18 +0700

    add IUserRepository

---
+++ Backend/DataAccessLayer/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DomainAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories.Interfaces
+{
+    internal class IUserRepository : IRepository<User>
+    {
+      
+
+   
+        Task<User?> GetByEmailAsync(string email);
+        Task<bool> IsEmailExistsAsync(string email);
+       
+        Task<bool> changePassword(int id, string newPassword);
+
+        public Task<IEnumerable<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<User> IRepository<User>.GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
